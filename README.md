24997c4243c9d90ca4228d532a36753f91e77f78
Erick Pinos <erickpinos@mit.edu>
2018-06-02 00:28:23 -0400

    moved jquery to js file

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ The Impact Summit Hackathon Project :)
 
 
 
-# from piechart
+# CSS Pie Chart Credits
 A Pen created at CodePen.io. You can find this one at https://codepen.io/FlyersPh9/pen/Bytpg.
 
  A pie chart designed completely in HTML & CSS created by Sean at: http://www.ohsean.net
--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-/* Code by Sean @ http://www.ohsean.net */
+$(document).ready(function(){
+	$("button").click(function(){
+		$.get("https://api.etherscan.io/api?module=account&action=balance&address=0xb215cfebb90d91b1d2f499843800d3105b1366fc&tag=latest&apikey=9R1SBAHHRX5138FBZWPWP9W8JRFRFV73AJ", function(data, status){
+//			alert("Ethereum Balance: " + data.result);
+	        document.getElementById("address1").innerHTML = data.result;
+	   	});
+	});
+});
+
+/* CSS Pie Chart Code by Sean @ http://www.ohsean.net */
